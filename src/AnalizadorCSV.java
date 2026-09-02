a4911f3690698565e80db05a44bee018130f7a9c
Luis Angel Lerma Coss <angelerma680@gmail.com>
2026-09-01 22:55:18 -0700

    Visualizacion de argumentos

--- src/AnalizadorCSV.java
+++ src/AnalizadorCSV.java
@@ -1,2 +1,10 @@
 public class AnalizadorCSV {
+
+    public static void main(String[] args) {
+        System.out.println("Numero de argumentos: " + args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
 }
